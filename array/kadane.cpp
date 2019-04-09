b22c8a8334ba1ca6bf00b90825949c1cdfb46d32
shubhampal39 <40192314+shubhampal39@users.noreply.github.com>
2019-04-09 22:23:13 +0530

    Create kadane.cpp
    Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum.
    
    kadane-algorithm (for negative no)

---
+++ array/kadane.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+        {cin>>a[i];}
+        
+         int max_so_far = a[0]; 
+   int curr_max = a[0]; 
+  
+        
+        for (int i = 1; i < n; i++) 
+   { 
+        curr_max = max(a[i], curr_max+a[i]); 
+        max_so_far = max(max_so_far, curr_max); 
+   } 
+        
+    cout<<max_so_far<<endl;        
+    }
+
+    
+    	return 0;
+}
